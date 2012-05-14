0f7694d631299996a6ea1630321ae0086df68308
Gregor Majcen <majcn.m@gmail.com>
2012-05-14 13:13:31 +0200

    netestiran FrmEvaluator

--- src/compiler/frames/FrmEvaluator.java
+++ src/compiler/frames/FrmEvaluator.java
@@ -4,14 +4,17 @@ import compiler.abstree.*;
 import compiler.abstree.tree.*;
 import compiler.semanal.*;
 
-public class FrmEvaluator extends AbsEmptyVisitor {
+public class FrmEvaluator implements AbsVisitor {
+
+	private boolean debug = true;
+	private FrmFrame curFrame;
 	
 	public void visit(AbsAtomType acceptor) {
 	}
-	
+
 	public void visit(AbsConstDecl acceptor) {
 	}
-	
+
 	public void visit(AbsFunDecl acceptor) {
 		FrmFrame frame = new FrmFrame(acceptor, SemDesc.getScope(acceptor));
 		for (AbsDecl decl : acceptor.pars.decls) {
@@ -20,18 +23,26 @@ public class FrmEvaluator extends AbsEmptyVisitor {
 				FrmArgAccess access = new FrmArgAccess(varDecl, frame);
 				FrmDesc.setAccess(varDecl, access);
 			}
+			decl.accept(this);
 		}
 		for (AbsDecl decl : acceptor.decls.decls) {
 			if (decl instanceof AbsVarDecl) {
 				AbsVarDecl varDecl = (AbsVarDecl)decl;
 				FrmLocAccess access = new FrmLocAccess(varDecl, frame);
+				frame.locVars.add(access);
 				FrmDesc.setAccess(varDecl, access);
 			}
 			decl.accept(this);
 		}
+		curFrame = frame;
+		acceptor.stmt.accept(this);
+		int typeSize = SemDesc.getActualType(acceptor.type).size();
+		if(frame.sizeArgs < typeSize)
+			frame.sizeArgs = typeSize;
+		if(debug) System.out.println(frame.sizeArgs);
 		FrmDesc.setFrame(acceptor, frame);
 	}
-	
+
 	public void visit(AbsProgram acceptor) {
 		for (AbsDecl decl : acceptor.decls.decls) {
 			if (decl instanceof AbsVarDecl) {
@@ -51,34 +62,162 @@ public class FrmEvaluator extends AbsEmptyVisitor {
 				FrmArgAccess access = new FrmArgAccess(varDecl, frame);
 				FrmDesc.setAccess(varDecl, access);
 			}
+			decl.accept(this);
 		}
 		for (AbsDecl decl : acceptor.decls.decls) {
 			if (decl instanceof AbsVarDecl) {
 				AbsVarDecl varDecl = (AbsVarDecl)decl;
 				FrmLocAccess access = new FrmLocAccess(varDecl, frame);
+				frame.locVars.add(access);
 				FrmDesc.setAccess(varDecl, access);
 			}
 			decl.accept(this);
 		}
+		curFrame = frame;
+		acceptor.stmt.accept(this);
+		if(debug) System.out.println(frame.sizeArgs);
 		FrmDesc.setFrame(acceptor, frame);
 	}
 	
 	public void visit(AbsRecordType acceptor) {
 		int offset = 0;
-		for (AbsDecl decl : acceptor.fields.decls)
+		for (AbsDecl decl : acceptor.fields.decls) {
 			if (decl instanceof AbsVarDecl) {
 				AbsVarDecl varDecl = (AbsVarDecl)decl;
 				FrmCmpAccess access = new FrmCmpAccess(varDecl, offset);
 				FrmDesc.setAccess(varDecl, access);
 				offset = offset + SemDesc.getActualType(varDecl.type).size();
 			}
+			decl.accept(this);
+		}
 	}
 	
 	public void visit(AbsTypeDecl acceptor) {
-	}
-	
-	public void visit(AbsVarDecl acceptor) {
 		acceptor.type.accept(this);
 	}
 	
+	public void visit(AbsVarDecl acceptor) {
+		if(acceptor.type instanceof AbsTypeName)
+			SemDesc.getNameDecl(acceptor.type).accept(this);
+		else
+			acceptor.type.accept(this);
+	}
+
+	@Override
+	public void visit(AbsAlloc acceptor) {
+		acceptor.type.accept(this);		
+	}
+
+	@Override
+	public void visit(AbsArrayType acceptor) {
+		acceptor.type.accept(this);
+	}
+
+	@Override
+	public void visit(AbsAssignStmt acceptor) {
+		acceptor.srcExpr.accept(this);
+		acceptor.dstExpr.accept(this);
+	}
+
+	@Override
+	public void visit(AbsAtomConst acceptor) {
+	}
+
+	@Override
+	public void visit(AbsBinExpr acceptor) {
+		acceptor.fstExpr.accept(this);
+		acceptor.sndExpr.accept(this);
+	}
+
+	@Override
+	public void visit(AbsBlockStmt acceptor) {
+		acceptor.stmts.accept(this);
+	}
+
+	@Override
+	public void visit(AbsCallExpr acceptor) {
+		int callArgsSize = 4;
+		for (AbsValExpr expr : acceptor.args.exprs) {
+			try {
+				callArgsSize += SemDesc.getActualType(expr).size();
+			} catch (NullPointerException e) {
+				if(debug) System.out.println("SemType not found");
+			}
+		}
+		if(curFrame.sizeArgs < callArgsSize)
+			curFrame.sizeArgs = callArgsSize;
+	}
+
+	@Override
+	public void visit(AbsDeclName acceptor) {
+	}
+
+	@Override
+	public void visit(AbsDecls acceptor) {
+		for (AbsDecl decl : acceptor.decls) {
+			decl.accept(this);
+		}
+	}
+
+	@Override
+	public void visit(AbsExprStmt acceptor) {
+		acceptor.expr.accept(this);		
+	}
+
+	@Override
+	public void visit(AbsForStmt acceptor) {
+		acceptor.name.accept(this);
+		acceptor.loBound.accept(this);
+		acceptor.hiBound.accept(this);
+	}
+
+	@Override
+	public void visit(AbsIfStmt acceptor) {
+		acceptor.cond.accept(this);
+		acceptor.thenStmt.accept(this);
+		acceptor.elseStmt.accept(this);	
+	}
+
+	@Override
+	public void visit(AbsNilConst acceptor) {
+	}
+
+	@Override
+	public void visit(AbsPointerType acceptor) {
+		acceptor.type.accept(this);
+	}
+
+	@Override
+	public void visit(AbsStmts acceptor) {
+		for (AbsStmt stmt : acceptor.stmts) {
+			stmt.accept(this);
+		}
+	}
+
+	@Override
+	public void visit(AbsTypeName acceptor) {
+	}
+
+	@Override
+	public void visit(AbsUnExpr acceptor) {
+		acceptor.expr.accept(this);
+	}
+
+	@Override
+	public void visit(AbsValExprs acceptor) {
+		for (AbsValExpr expr : acceptor.exprs) {
+			expr.accept(this);
+		}
+	}
+
+	@Override
+	public void visit(AbsValName acceptor) {
+		// TODO poglej
+	}
+
+	@Override
+	public void visit(AbsWhileStmt acceptor) {
+		acceptor.cond.accept(this);
+		acceptor.stmt.accept(this);
+	}
 }
